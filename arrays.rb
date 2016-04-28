4701b8d2213ed877023b997eaef4944fd4292b26
John McIntosh II <jmci10@hotmail.com>
2016-04-28 12:05:57 +0000

    split strings into arrays on characters

--- arrays.rb
+++ arrays.rb
@@ -1,3 +1,11 @@
 print "foo bar baz\n"
 print "after splitting...\n"
-print "foo bar baz".split
+print "foo bar baz".split # ["foo", "bar", "baz"]
+
+print "\n"
+
+print "split on a character...\n"
+print "fooxbarxbazx".split('x') # ["foo", "bar", "baz"]
+
+
+
