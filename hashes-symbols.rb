23e10fe4d83f36ad61613721fc8ab6a56dff4b10
John McIntosh II <jmci10@hotmail.com>
2016-05-19 10:46:37 +0000

    start hashes and symbols

---
+++ hashes-symbols.rb
@@ -0,0 +1,14 @@
+# empty hash
+user = {}
+
+# key "first_name", value "michael"
+user["first_name"] = "Michael"
+
+# key "last_name", value "hartl"
+user["last_name"] = "Hartl"
+
+# element access is like arrays
+user["first_name"]
+
+# literal representation of a hash
+{ "last_name"=>"Hartl", "first_name"=>"Michael" }
