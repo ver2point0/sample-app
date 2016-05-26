9bf0ecf0fcc8754379834f01d62399bb1e87163c
John McIntosh II <jmci10@hotmail.com>
2016-05-26 13:13:03 +0000

    hashes with each method

--- hashes-symbols.rb
+++ hashes-symbols.rb
@@ -39,7 +39,10 @@ params[:user] = { name: "Michael Hartl", email: "mhartl@example.com" }
 params
 params[:user][:email]
 
-
+flash = { success: "It worked!", danger: "It failed." }
+flash.each do |key, value|
+  puts "Key #{key.inspect} has value #{value.inspect}"
+end
 
 
 
