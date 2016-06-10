1ebfcd272fa81933297eabaff70a2342b8b084b3
John McIntosh II <jmci10@hotmail.com>
2016-06-10 13:30:30 +0000

    create & examine controller class heirarchy

--- controller-class.rb
+++ controller-class.rb
@@ -9,3 +9,11 @@ class StaticPagesController < ApplicationController
   def about
   end
 end
+
+controller = StaticPagesController.new
+controller.class
+controller.class.superclass
+controller.class.superclass.superclass
+controller.class.superclass.superclass.superclass
+controller.class.superclass.superclass.superclass.superclass
+controller.class.superclass.superclass.superclass.superclass.superclass
