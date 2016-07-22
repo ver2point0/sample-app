522226a79bece8af36e62b0ad4d3953ca8f600c4
John McIntosh II <jmci10@hotmail.com>
2016-07-22 19:51:15 +0000

    finishing adding contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,24 +2,26 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get :home
+    get static_pages_home_url
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get :help
+    get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get about" do
-    get :about
+    get static_pages_about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get contact" do
-    
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
