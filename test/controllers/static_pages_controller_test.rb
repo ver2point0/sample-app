6777284551154efdf4b709f3190210ebee3f4fea
John McIntosh II <jmci10@hotmail.com>
2016-07-23 16:47:33 +0000

    fix static pages controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,25 +2,25 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get static_pages_home_url
+    get :home
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get :help
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get contact" do
-    get static_pages_contact_url
+    get :contact
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
