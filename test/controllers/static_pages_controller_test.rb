0068c01f7f12c4c162aeeba81bf488160c83e573
John McIntosh II <jmci10@hotmail.com>
2016-07-25 16:47:37 +0000

    change routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,25 +2,25 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get :home
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get :help
+    get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get about" do
-    get :about
+    get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get contact" do
-    get :contact
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
