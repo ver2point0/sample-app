6e0f4e80193c47ddc2538a789449e2541f56bb84
John McIntosh II <jmci10@hotmail.com>
2016-07-29 13:26:56 +0000

    update footer partial with named routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,25 +2,25 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get root_path
+    get :home
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get help_path
+    get :help
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get about" do
-    get about_path
+    get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get contact" do
-    get contact_path
+    get :contact
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
