113773f0d535d7df6ce99dec5c145be53d4a0098
John McIntosh II <jmci10@hotmail.com>
2016-08-04 14:44:00 +0000

    start adding integration assertions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  get root_path
+  assert_template 'static_pages/home'
+  assert_select "a[href=?]", root_path, count: 2
+  assert_select "a[href=?]", help_path
 end
