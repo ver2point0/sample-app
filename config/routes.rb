cde5186f968aa33163b2baff495ef67ffed30465
John McIntosh II <jmci10@hotmail.com>
2016-08-09 00:47:21 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
 end
