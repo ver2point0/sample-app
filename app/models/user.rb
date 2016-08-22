e2e462de4c4a15a0442ec1b1c54b87ec81e503c0
John McIntosh II <jmci10@hotmail.com>
2016-08-22 13:26:26 +0000

    validate length of username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 }
+    
 end
