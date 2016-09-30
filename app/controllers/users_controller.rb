076439f1dd834bc9a10073dc26ddc8613c149f76
John McIntosh II <jmci10@hotmail.com>
2016-09-30 01:38:47 +0000

    add the create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # handle a successful save
+    else
+      render "new"
+    end
+  end
+  
 end
