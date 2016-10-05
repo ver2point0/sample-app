b31a9f133340d5790839c4f48207fcbb56f5b3df
John McIntosh II <jmci10@hotmail.com>
2016-10-05 20:18:53 +0000

    add user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       # handle a successful save
     else
@@ -17,4 +17,10 @@ class UsersController < ApplicationController
     end
   end
   
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
